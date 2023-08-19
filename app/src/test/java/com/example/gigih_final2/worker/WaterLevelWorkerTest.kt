46a2c5266ee5645e018d555d491371025cc9362c
DysnomiaBorealis <dkphobo@gmail.com>
2023-08-20 06:24:38 +0700

    adding few test cases

--- app/src/test/java/com/example/gigih_final2/worker/WaterLevelWorkerTest.kt
+++ app/src/test/java/com/example/gigih_final2/worker/WaterLevelWorkerTest.kt
@@ -2,42 +2,57 @@ package com.example.gigih_final2.worker
 
 import android.app.NotificationManager
 import android.content.Context
+import androidx.work.Data
 import androidx.work.WorkerParameters
+import androidx.work.testing.TestListenableWorkerBuilder
 import com.example.gigih_final2.domain.UseCase.WaterLevelUseCase
 import com.example.gigih_final2.domain.Worker.WaterLevelWorker
 import com.example.gigih_final2.utils.ResultState
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import java.util.*
 
+import org.robolectric.RobolectricTestRunner
+
+
+@RunWith(RobolectricTestRunner::class)
 class WaterLevelWorkerTest {
 
-    @Mock
-    private lateinit var mockContext: Context
 
     @Mock
-    private lateinit var mockWorkerParams: WorkerParameters
-
+    private lateinit var workerParams: WorkerParameters
     @Mock
-    private lateinit var mockNotificationManager: NotificationManager
-
+    private lateinit var context: Context
     @Mock
-    private lateinit var mockWaterLevelUseCase: WaterLevelUseCase
+    private lateinit var notificationManager: NotificationManager
+    @Mock
+    private lateinit var waterLevelUseCase: WaterLevelUseCase
 
     private lateinit var worker: WaterLevelWorker
 
     @Before
     fun setUp() {
         MockitoAnnotations.openMocks(this)
-        worker = WaterLevelWorker(mockContext, mockWorkerParams, mockNotificationManager, mockWaterLevelUseCase)
-    }
 
+        // Mock the WorkerParameters
+        Mockito.`when`(workerParams.inputData).thenReturn(Data.EMPTY)
+        Mockito.`when`(workerParams.tags).thenReturn(emptySet())
+        Mockito.`when`(workerParams.runAttemptCount).thenReturn(0)
+        Mockito.`when`(workerParams.id).thenReturn(UUID.randomUUID())
+        Mockito.`when`(workerParams.network).thenReturn(null)
+        Mockito.`when`(workerParams.triggeredContentUris).thenReturn(emptyList())
+        Mockito.`when`(workerParams.triggeredContentAuthorities).thenReturn(emptyList())
+
+        worker = TestListenableWorkerBuilder<WaterLevelWorker>(context).build()
+    }
     @Test
     fun `test doWork with success result`() = runBlocking {
-        Mockito.`when`(mockWaterLevelUseCase.checkWaterLevelThreshold()).thenReturn(ResultState.Success(true))
+        Mockito.`when`(waterLevelUseCase.checkWaterLevelThreshold()).thenReturn(ResultState.Success(true))
 
         val result = worker.doWork()
 
@@ -47,12 +62,13 @@ class WaterLevelWorkerTest {
     @Test
     fun `test doWork with error result`() = runBlocking {
         val mockException = Exception("Error")
-        Mockito.`when`(mockWaterLevelUseCase.checkWaterLevelThreshold()).thenReturn(ResultState.Error(mockException))
+        Mockito.`when`(waterLevelUseCase.checkWaterLevelThreshold()).thenReturn(ResultState.Error(mockException))
 
         val result = worker.doWork()
 
         assert(result == androidx.work.ListenableWorker.Result.failure())
     }
+
 
 
 }
